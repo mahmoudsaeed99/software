4b5279d90f24d9967995f386209ba3835ac07ec4
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2019-12-27 00:44:39 +0200

    add classes

--- SoftWareAPI/src/softwareapi/User.java
+++ SoftWareAPI/src/softwareapi/User.java
@@ -11,30 +11,31 @@ import java.util.ArrayList;
  *
  * @author COMPU1
  */
-public class User {
+abstract class User {
 
     public String name;
     public String pwd;
     public String mail;
+    public boolean checkUserType;
     public ArrayList<User> friendList;
     public ArrayList<User> friendRequest;
     public ArrayList<Group> groups;
     public ArrayList<Page> pages;
-    private ArrayList<Post>posts;
-    private ArrayList<Hashtag>hashtags;
+    private ArrayList<Post> posts;
+    private ArrayList<Hashtag> hashtags;
     Adminstrator admin = new Adminstrator();
 
     public User() {
         name = new String();
         pwd = new String();
         mail = new String();
+        checkUserType = false;
         friendList = new ArrayList<>();
         System.out.println("orabi");
     }
 
     public User(Adminstrator admin) {
-            this.admin=admin;
+        this.admin = admin;
     }
 
-    
 }
---
+++ SoftWareAPI/src/softwareapi/premium.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softwareapi;
+
+/**
+ *
+ * @author mahmoudsaeed
+ */
+public class premium extends User{
+
+    public premium() {
+
+    }
+
+    public void updateProfile(String name, String pwd, String mail) {
+        User newUser = new premium();
+        newUser.mail = mail;
+        newUser.name = name;
+        newUser.pwd = pwd;
+        //admin.signUpValidate(this);
+        if (admin.updateInfo(this, newUser)) {
+            this.name = name;
+            this.pwd = pwd;
+            this.mail = mail;
+            System.out.println("update successfully");
+        } else {
+            System.out.println("update faild");
+        }
+    }
+
+}
