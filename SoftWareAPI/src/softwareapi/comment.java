eed58bd798f2e5639df974914954485541543123
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2019-12-27 00:29:22 +0200

    add comment class

--- SoftWareAPI/src/softwareapi/comment.java
+++ SoftWareAPI/src/softwareapi/comment.java
@@ -1,4 +1,4 @@
-/*
+   /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,5 +10,12 @@ package softwareapi;
  * @author C L
  */
 public class comment {
+    User owner;
+    String conent;
+    public comment makeComment(User owner , String content){
+        this.conent = content;
+        this.owner = owner;
+        return this;
+    }
     
 }
