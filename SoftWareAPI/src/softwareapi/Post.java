282a76c1db912bdb3cf77ca98d49b853b5b4d7b8
mohamed123 <mohamedmagdy7770@gmail.com>
2019-12-27 01:06:20 +0200

    post modified

--- SoftWareAPI/src/softwareapi/Post.java
+++ SoftWareAPI/src/softwareapi/Post.java
@@ -1,14 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package softwareapi;
 
-/**
- *
- * @author mahmoudsaeed
- */
+import java.util.ArrayList;
+
 public class Post {
-    
+
+    public String content;
+    public User owner;
+    public ArrayList<User> likes = new ArrayList<>();
+    public ArrayList<comment> comments = new ArrayList<>();
+
+    public Post writepost(String content, User user) {
+        this.content = content;
+        this.owner = user;
+        return this;
+
+    }
+
+    public void makeLike(User user) {
+        likes.add(user);
+    }
+
+    public void makeComment(User user, String content) {
+        comment comment = new comment();
+        comments.add(comment.makeComment(owner, content));
+    }
+
 }
