9b3fbc4ab032eb64b92e45d8822c7da3109991ff
mohamed123 <mohamedmagdy7770@gmail.com>
2019-12-27 01:08:01 +0200

    hashtag modified

--- SoftWareAPI/src/softwareapi/Hashtag.java
+++ SoftWareAPI/src/softwareapi/Hashtag.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package softwareapi;
 
-/**
- *
- * @author mahmoudsaeed
- */
+import java.util.ArrayList;
+
 public class Hashtag {
-    
+
+    public String name;
+    public ArrayList<Post> posts;
+
+    public Hashtag() {
+        this.posts = new ArrayList();
+    }
+
+    public Hashtag(String name) {
+        this.name = name;
+        this.posts = new ArrayList();
+
+    }
+
+    public Hashtag writeHshtag(String name, Post post) {
+        this.name = name;
+        this.posts.add(post);
+        return this;
+
+    }
+
+    public void addPost(Post post) {
+        this.posts.add(post);
+    }
+
 }
