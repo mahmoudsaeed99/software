e59e53ec8c190e1bc6322daff974c5e8fff9fed8
mahmoudsaeed99 <36303424+mahmoudsaeed99@users.noreply.github.com>
2019-12-27 01:30:32 +0200

    page

--- SoftWareAPI/src/softwareapi/Page.java
+++ SoftWareAPI/src/softwareapi/Page.java
@@ -5,10 +5,58 @@
  */
 package softwareapi;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mahmoudsaeed
  */
-public class Page {
-    
+public class Page implements controler {
+    public User admin;
+    public String name;
+    public String type;
+    public ArrayList<User> followers ;
+    public ArrayList<Post> posts ;
+    public ArrayList<Hashtag> hashtags ;
+
+    public Page() {
+        admin = new User() {};
+        type = new String();
+        followers = new ArrayList<>();
+        posts = new ArrayList<>();
+        name = new String();
+    }
+
+    public Page(User admin, String type , String nameOfPage) {
+        this.admin = admin;
+        this.type = type;
+        this.name = nameOfPage;
+    }
+
+  
+
+    @Override
+    public void createPost(String content, User owner) {
+         Post post = new Post();
+        posts.add(post.writepost(content, owner));
+       // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createHashtag(String name, Post post) {
+        boolean find = false;
+        for(int i=0;i<hashtags.size();i++){
+            if(name.equals(hashtags.get(i))){
+                hashtags.get(i).addPost(post);
+                find = true;
+            }
+        }
+        if(!find){
+            Hashtag hashtag = new Hashtag();
+            hashtag.writeHshtag(name, post);
+            hashtags.add(hashtag);
+        }
+        
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
